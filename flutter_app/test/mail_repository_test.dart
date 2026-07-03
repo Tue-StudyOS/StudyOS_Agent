cb86667eb638c03e806d58fd79fab6ffdf512980
linuscooper <linuscooper@web.de>
2026-07-03 17:45:07 +0200

    Fix mail_repository_test.dart

--- flutter_app/test/mail_repository_test.dart
+++ flutter_app/test/mail_repository_test.dart
@@ -1,10 +1,19 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 import 'package:studyos_agent/src/mail_client.dart';
 import 'package:studyos_agent/src/mail_repository.dart';
 import 'package:studyos_agent/src/models.dart';
 import 'package:studyos_agent/src/profile_store.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferencesAsyncPlatform.instance =
+        InMemorySharedPreferencesAsync.empty();
+    FlutterSecureStorage.setMockInitialValues(<String, String>{});
+  });
+
   test('mailbox snapshot reuses one authenticated client', () async {
     final client = _CountingMailClient();
     final repository = MailRepository.test(
